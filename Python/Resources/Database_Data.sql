670529bf40058bd7adcf285c7cc4bf1bd19b999e
Mommer023 <taylormommer@icloud.com>
2023-12-02 14:30:31 -0600

    Add Products

--- Python/Resources/Database_Data.sql
+++ Python/Resources/Database_Data.sql
@@ -97,7 +97,20 @@ INSERT INTO Marketing(marketing_strategy, marketing_budget, marketing_campaign)
 
 SELECT * FROM Marketing;
 #Product Table Data:
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Outdoor Hiking Backpack", 45.00, 12);
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Trekking Poles", 39.99, 10);
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Tent", 248.39, 15);
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Camping Survival Kit", 45.87, 23);
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Cooler", 179.99, 10);
+INSERT INTO Product(product_name, product_price, product_quantity)
+    VALUES("Hammock", 39.99, 23);
 
+Select * FROM Product;
 #Registration Table Data:
 
 #Trip Table Data:
